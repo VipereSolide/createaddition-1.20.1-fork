bb92f9b54e2cc9604e6609b4949b47e22f7312d7
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2024-01-07 22:27:34 +0100

    Added the Labelling Machine.

--- src/main/java/com/rose/gods_retribution/content/AllBlocks.java
+++ src/main/java/com/rose/gods_retribution/content/AllBlocks.java
@@ -1,6 +1,11 @@
 package com.rose.gods_retribution.content;
 
+import com.rose.gods_retribution.content.block.labelling_machine.LabellingMachineBlock;
+import com.simibubi.create.content.kinetics.BlockStressDefaults;
+import com.simibubi.create.foundation.data.AssetLookup;
+import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -11,6 +16,17 @@ import static com.rose.gods_retribution.GodsRetribution.REGISTRATE;
 import static com.simibubi.create.foundation.data.BlockStateGen.simpleCubeAll;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 import static com.simibubi.create.foundation.data.TagGen.tagBlockAndItem;
+import static com.simibubi.create.content.redstone.displayLink.AllDisplayBehaviours.assignDataBehaviour;
+import static com.simibubi.create.foundation.data.BlockStateGen.axisBlock;
+import static com.simibubi.create.foundation.data.BlockStateGen.simpleCubeAll;
+import static com.simibubi.create.foundation.data.CreateRegistrate.connectedTextures;
+import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
+import static com.simibubi.create.foundation.data.TagGen.axeOnly;
+import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
+import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
+import static com.simibubi.create.foundation.data.TagGen.tagBlockAndItem;
+
+import com.simibubi.create.content.processing.AssemblyOperatorBlockItem;
 
 public class AllBlocks
 {
@@ -32,6 +48,19 @@ public class AllBlocks
             .tab(AllCreativeTabs.MAIN.getKey())
             .build()
             .register();
+
+    public static final BlockEntry<LabellingMachineBlock> LABELLING_MACHINE_BLOCK =
+            REGISTRATE.block("labelling_machine", LabellingMachineBlock::new)
+                    .initialProperties(SharedProperties::stone)
+                    .properties(p -> p.noOcclusion().mapColor(MapColor.STONE))
+                    .transform(axeOrPickaxe())
+                    .blockstate((c, p) -> p.simpleBlock(c.getEntry(), AssetLookup.partialBaseModel(c, p)))
+                    .addLayer(() -> RenderType::cutoutMipped)
+                    .transform(BlockStressDefaults.setImpact(4.0))
+                    .item(AssemblyOperatorBlockItem::new)
+                    .tab(AllCreativeTabs.MAIN.getKey())
+                    .transform(customItemModel())
+                    .register();
 
     /**
      * Loads this class
---
+++ src/main/java/com/rose/gods_retribution/content/block/labelling_machine/LabellingMachineBlock.java
@@ -0,0 +1,11 @@
+package com.rose.gods_retribution.content.block.labelling_machine;
+
+import net.minecraft.world.level.block.Block;
+
+public class LabellingMachineBlock extends Block
+{
+    public LabellingMachineBlock(Properties pProperties)
+    {
+        super(pProperties);
+    }
+}
