e7b2528f0219c3c00063afe624520c4586251ceb
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2024-01-07 12:53:53 +0100

    Cleaning

---
+++ src/main/java/com/rose/gods_retribution/CreateAddition.java
@@ -0,0 +1,89 @@
+package com.rose.gods_retribution;
+
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipHelper;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.TooltipModifier;
+
+@Mod(CreateAddition.MOD_ID)
+public class CreateAddition {
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "gods_retribution";
+
+    public static boolean IMMERSIVE_ENGINEERING_ACTIVE = false;
+    public static boolean COMPUTER_CRAFT_ACTIVE = false;
+    public static boolean AE2_ACTIVE = false;
+
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(CreateAddition.MOD_ID);
+
+    private static final String PROTOCOL = "1";
+	public static final SimpleChannel Network = NetworkRegistry.ChannelBuilder.named(new ResourceLocation(MOD_ID, "main"))
+            .clientAcceptedVersions(PROTOCOL::equals)
+            .serverAcceptedVersions(PROTOCOL::equals)
+            .networkProtocolVersion(() -> PROTOCOL)
+            .simpleChannel();
+
+    static {
+        REGISTRATE.setTooltipModifierFactory(item -> new ItemDescription.Modifier(item, TooltipHelper.Palette.STANDARD_CREATE)
+                .andThen(TooltipModifier.mapNull(KineticStats.create(item))));
+    }
+
+    public CreateAddition() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::postInit);
+        //FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(RecipeSerializer.class, CARecipes::register);
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
+        Config.loadConfig(Config.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve("createaddition-common.toml"));
+
+        IMMERSIVE_ENGINEERING_ACTIVE = ModList.get().isLoaded("immersiveengineering");
+        COMPUTER_CRAFT_ACTIVE = ModList.get().isLoaded("computercraft");
+        AE2_ACTIVE = ModList.get().isLoaded("ae2");
+
+        REGISTRATE.registerEventListeners(eventBus);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+    public void postInit(FMLLoadCompleteEvent evt) {
+    	System.out.println("Gods' Retribution Initialized!");
+    }
+
+    @SubscribeEvent
+    public void onRegisterCommandEvent(RegisterCommandsEvent event) {
+    }
+
+    public static ResourceLocation asResource(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
+}
