7fa158be5696a4115b418316084616dc7c3dbca0
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2024-01-08 08:54:46 +0100

    Connected the labelling machine to Create and started adding the behaviour.

--- src/main/java/com/rose/gods_retribution/content/block/labelling_machine/LabellingMachineBlock.java
+++ src/main/java/com/rose/gods_retribution/content/block/labelling_machine/LabellingMachineBlock.java
@@ -1,11 +1,50 @@
 package com.rose.gods_retribution.content.block.labelling_machine;
 
-import net.minecraft.world.level.block.Block;
+import com.simibubi.create.content.kinetics.base.HorizontalKineticBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 
-public class LabellingMachineBlock extends Block
+public class LabellingMachineBlock extends HorizontalKineticBlock
 {
+    // TODO: Add the block entity.
+
     public LabellingMachineBlock(Properties pProperties)
     {
         super(pProperties);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        Direction preferredSide = getPreferredHorizontalFacing(context);
+        if (preferredSide != null)
+            return defaultBlockState().setValue(HORIZONTAL_FACING, preferredSide);
+
+        return super.getStateForPlacement(context);
+    }
+
+    @Override
+    public Direction.Axis getRotationAxis(BlockState state)
+    {
+        return state.getValue(HORIZONTAL_FACING)
+                .getAxis();
+    }
+
+    @Override
+    public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face)
+    {
+        return face.getAxis() == state.getValue(HORIZONTAL_FACING)
+                .getAxis();
+    }
+
+    @Override
+    public boolean isPathfindable(BlockState state, BlockGetter reader, BlockPos pos, PathComputationType type)
+    {
+        return false;
     }
 }
---
+++ src/main/java/com/rose/gods_retribution/content/block/labelling_machine/LabellingMachineBlockEntity.java
@@ -0,0 +1,6 @@
+package com.rose.gods_retribution.content.block.labelling_machine;
+
+public class LabellingMachineBlockEntity
+{
+    // TODO: Write this class UwU
+}
