6b0cfc888d04cefad173f194f9b86f1415a777d8
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2024-01-16 18:51:35 +0100

    Added a way to register blocks with directions easily.

---
+++ src/main/java/com/rose/gods_retribution/fundation/blocks/RegisteredHorizontalDirectionalBlock.java
@@ -0,0 +1,11 @@
+package com.rose.gods_retribution.fundation.blocks;
+
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+
+public class RegisteredHorizontalDirectionalBlock extends HorizontalDirectionalBlock
+{
+    protected RegisteredHorizontalDirectionalBlock(Properties pProperties)
+    {
+        super(pProperties);
+    }
+}
