ebd0553217756a00547d20fc1299d64071b00c63
Blenden Mark <none>
2024-01-20 13:57:20 +0100

    Added architecture for custom tags

---
+++ src/main/java/com/rose/gods_retribution/content/AllTags.java
@@ -0,0 +1,56 @@
+package com.rose.gods_retribution.content;
+
+import com.rose.gods_retribution.GodsRetribution;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+/**
+ * This class contains references to the custom tags added by the mod. Beware : adding a tag in
+ * here does not create a tag ingame. You have to create the tags in the data generation tags
+ * provider, then reference it here.
+ */
+public class AllTags
+{
+	/**
+	 * The inner class for the blocks' tags.
+	 */
+	public static class Blocks
+	{
+		/**
+		 * Only for demonstration purposes.
+		 */
+		public static final TagKey<Block> WOODY = tag("woody");
+
+		/**
+		 * Helper method used to create the blocks' tags in this very class.
+		 *
+		 * @param name the tag's ID
+		 * @return the tag
+		 */
+		private static TagKey<Block> tag(String name)
+		{
+			return BlockTags.create(new ResourceLocation(GodsRetribution.MOD_ID, name));
+		}
+	}
+
+	/**
+	 * The inner class for the items' tags.
+	 */
+	public static class Items
+	{
+		/**
+		 * Helper method used to create the items' tags in this very class.
+		 *
+		 * @param name the tag's ID
+		 * @return the tag
+		 */
+		private static TagKey<Item> tag(String name)
+		{
+			return ItemTags.create(new ResourceLocation(GodsRetribution.MOD_ID, name));
+		}
+	}
+}
