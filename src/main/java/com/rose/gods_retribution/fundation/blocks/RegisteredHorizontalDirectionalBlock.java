e4602e578db7f644137de64f5a4377abcb3e21e5
Blenden Mark <none>
2024-01-28 10:48:15 +0100

    Refactoring of the RegisteredHorizontalDirectionalBlock class

--- src/main/java/com/rose/gods_retribution/fundation/blocks/RegisteredHorizontalDirectionalBlock.java
+++ src/main/java/com/rose/gods_retribution/fundation/blocks/RegisteredHorizontalDirectionalBlock.java
@@ -1,11 +1,47 @@
 package com.rose.gods_retribution.fundation.blocks;
 
+import com.simibubi.create.foundation.utility.VoxelShaper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class RegisteredHorizontalDirectionalBlock extends HorizontalDirectionalBlock
 {
+    protected VoxelShaper _shape;
+
     public RegisteredHorizontalDirectionalBlock(Properties pProperties)
     {
         super(pProperties);
+    }
+
+    public RegisteredHorizontalDirectionalBlock(VoxelShaper shape, Properties properties)
+    {
+        super(properties);
+        this._shape = shape;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext)
+    {
+        return this._shape.get(pState.getValue(FACING));
     }
 }
